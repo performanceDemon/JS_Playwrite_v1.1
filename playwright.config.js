62f7a6b48dfdd52b79d38298833ff34bda40ad29
emmanuel isaac Garcia Cruz <isaac.garcia.cruz@gmail.com>
2024-08-06 13:27:49 -0600

    Actualizado para ejecutarse en 3 navegadores google, safari, firefox

---
+++ playwright.config.js
@@ -0,0 +1,28 @@
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+  retries: 2,
+  reporter: 'list',
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 0,
+    ignoreHTTPSErrors: true,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
---
+++ tests/tests/login.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+const { test, expect } = require('@playwright/test');
+require('dotenv').config();
+
+
+test('login test', async ({ page }) => {
+  await page.goto('https://medium.com/');
+  await page.getByRole('link', { name: 'Sign in' }).click();
+  await page.getByRole('button', { name: 'Sign in with email' }).click();
+  await page.getByRole('textbox').click();
+  await page.getByRole('textbox').fill('idsds.falso');
+  await page.getByRole('button', { name: 'Continue' }).click();
+  await page.getByText('Please enter a valid email').click();
+  await page.getByRole('button', { name: 'Continue' }).click();
+  await page.getByRole('heading', { name: 'Sign in with email' }).click();
+  await page.getByRole('heading', { name: 'Sign in with email' }).click();
+  await page.getByRole('button', { name: 'All sign in options' }).click();
+});
